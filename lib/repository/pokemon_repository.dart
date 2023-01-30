1bc6d3307da4d2cbf94fc661df5086ea55ebbba2
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-01-30 19:13:24 -0300

    colocando os pokemon na tela

--- lib/repository/pokemon_repository.dart
+++ lib/repository/pokemon_repository.dart
@@ -2,10 +2,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-import 'package:pokedex/repository/pokemon_model.dart';
+import 'package:pokedex/repository/pokemon_list__model.dart';
 
 class PokemonRepository {
-  Future<List<dynamic>> getPokemon({
+  Future<Map<String, dynamic>> getPokemonList({
     String limit = '100000',
     String offset = '0',
   }) async {
@@ -20,11 +20,13 @@ class PokemonRepository {
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
-      Map<String, dynamic> map = json.decode(response.body);
-      List pokemon = map['results'];
-      pokemon = pokemon.map((json) => PokemonModel.fromJson(json)).toList();
+      // Map<String, dynamic> map = json.decode(response.body);
+      // List a = map['results'];
+      // a = a.map((json) => PokemonModel.fromJson(json)).toList();
 
-      return pokemon;
+      var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+
+      return jsonResponse;
     } else {
       throw ('ERRO');
     }
